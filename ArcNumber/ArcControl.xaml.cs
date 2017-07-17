76d5c760f06be523bea07f51207a863c40f2086b
John Pomeroy <jpcoded@gmail.com>
2017-07-17 19:40:11 -0400

    Add project files.

---
+++ ArcNumber/ArcControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace ArcNumber
+{
+    /// <summary>
+    /// Interaction logic for ArcControl.xaml
+    /// </summary>
+    public partial class ArcControl : UserControl
+    {
+        int _arcNum = 0;
+        private readonly List<Path> _arcs;
+        public ArcControl()
+        {
+            InitializeComponent();
+            _arcs = new List<Path> { arcZero,arcOne,arcTwo,arcThree,arcFour,arcFive,arcSix,arcSeven,arcEight,arcNine };
+
+        }
+
+        public void SetArc(int num)
+        {
+            foreach(var arc in _arcs)
+            {
+                arc.Visibility = Visibility.Hidden;                                                                                   
+            }
+
+            _arcs[num].Visibility = Visibility.Visible;
+        }
+
+        public void IncreaseArc()
+        {
+            _arcs[_arcNum].Visibility = Visibility.Hidden;
+            if (_arcNum + 1 > 9)
+            {
+                _arcNum = 0;
+            }
+            else
+            {
+                _arcNum++;
+            }
+      
+           
+            _arcs[_arcNum].Visibility = Visibility.Visible;
+            
+        }
+    }
+}
+                                                                                                                                                                                        
---
+++ ArcNumber/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace ArcNumber
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            acSegment.SetArc(0);
+        }
+
+        private void btnIncrease_Click(object sender, RoutedEventArgs e)
+        {
+            acSegment.IncreaseArc();
+        }
+    }
+}
