e67a4fb178e88185767d187cb8133d1351365b32
John Pomeroy <jpcoded@gmail.com>
2017-07-20 17:30:08 -0400

    Added a bolt that will be used for apperanceds, and change the numbers angles to look better and to make them change to grey when not being used

--- ArcNumber/ArcControl.xaml.cs
+++ ArcNumber/ArcControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace ArcNumber
@@ -12,26 +13,33 @@ namespace ArcNumber
     {
         int _arcNum = 0;
         private readonly List<Path> _arcs;
+        private readonly List<Label> _numbers;
+
         public ArcControl()
         {
             InitializeComponent();
             _arcs = new List<Path> { arcZero,arcOne,arcTwo,arcThree,arcFour,arcFive,arcSix,arcSeven,arcEight,arcNine };
-
+            _numbers = new List<Label> { lblZero, lblOne, lblTwo, lblThree, lblFour, lblFive, lblSix, lblSeven, lblEight, lblNine };
+            foreach (var num in _numbers)
+            {
+                num.Foreground = Brushes.Gray;
+            }
+            foreach (var arc in _arcs)
+            {
+                arc.Visibility = Visibility.Hidden;                                                                                   
+            }  
         }
 
         public void SetArc(int num)
         {
-            foreach(var arc in _arcs)
-            {
-                arc.Visibility = Visibility.Hidden;                                                                                   
-            }  
-
-            _arcs[num].Visibility = Visibility.Visible;
+               _arcs[num].Visibility = Visibility.Visible;
         }
 
         public void IncreaseArc()
         {
             _arcs[_arcNum].Visibility = Visibility.Hidden;
+            _numbers[_arcNum].Foreground = Brushes.Gray;
+
             if (_arcNum + 1 > 9)
             {
                 _arcNum = 0;
@@ -40,8 +48,8 @@ namespace ArcNumber
             {
                 _arcNum++;
             }
-      
-           
+
+            _numbers[_arcNum].Foreground = Brushes.Red;
             _arcs[_arcNum].Visibility = Visibility.Visible;
             
         }
