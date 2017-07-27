c99dc6f7151e83236913c45a8c7a81fe7a90cff1
John Pomeroy <jpcoded@gmail.com>
2017-07-27 15:29:44 -0400

    Added more resouces to dictonary

--- ArcNumber/ArcControl.xaml.cs
+++ ArcNumber/ArcControl.xaml.cs
@@ -14,10 +14,13 @@ namespace ArcNumber
         int _arcNum = 0;
         private readonly List<Path> _arcs;
         private readonly List<Label> _numbers;
+        
 
         public ArcControl()
         {
             InitializeComponent();
+
+           
             _arcs = new List<Path> { arcZero,arcOne,arcTwo,arcThree,arcFour,arcFive,arcSix,arcSeven,arcEight,arcNine };
             _numbers = new List<Label> { lblZero, lblOne, lblTwo, lblThree, lblFour, lblFive, lblSix, lblSeven, lblEight, lblNine };
             foreach (var num in _numbers)
@@ -33,6 +36,7 @@ namespace ArcNumber
         public void SetArc(int num)
         {
                _arcs[num].Visibility = Visibility.Visible;
+            _numbers[num].Foreground = Brushes.Red;
         }
 
         public void IncreaseArc()
@@ -40,14 +44,8 @@ namespace ArcNumber
             _arcs[_arcNum].Visibility = Visibility.Hidden;
             _numbers[_arcNum].Foreground = Brushes.Gray;
 
-            if (_arcNum + 1 > 9)
-            {
-                _arcNum = 0;
-            }
-            else
-            {
-                _arcNum++;
-            }
+            _arcNum = (_arcNum + 1 > 9) ? 0 : _arcNum + 1;
+     
 
             _numbers[_arcNum].Foreground = Brushes.Red;
             _arcs[_arcNum].Visibility = Visibility.Visible;
