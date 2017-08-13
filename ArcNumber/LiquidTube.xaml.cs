7b80ca6f582d08768dfcbce5578ea53f0b0906a2
John Pomeroy <jpcoded@gmail.com>
2017-08-13 14:49:40 -0400

    Added offset to the creation of gradient

--- ArcNumber/LiquidTube.xaml.cs
+++ ArcNumber/LiquidTube.xaml.cs
@@ -17,23 +17,25 @@ namespace ArcNumber
 
         private async void AsyncUpdateLiquid()
         {
+     
             var LiquidBrush = new LinearGradientBrush()
             {
                 StartPoint = new System.Windows.Point(0.5, 0),
                 EndPoint = new System.Windows.Point(0.5, 1)
             };
-          
+
             var Lime = new GradientStop()
             {
-                Color = Colors.Lime
+                Color = Colors.Lime,
+                Offset = 1
             };
-            Lime.Offset = 1;
+         
             var White = new GradientStop()
             {
-                Color = Colors.White
-                
+                Color = Colors.White,
+                Offset = 1
             };
-            White.Offset = 0.90;
+      
 
             while (true)
             {
@@ -45,7 +47,7 @@ namespace ArcNumber
 
                 recLiquid.Fill = LiquidBrush;
                 if(White.Offset <= 0)
-                { White.Offset = .90;
+                { White.Offset = 1;
                     Lime.Offset = 1;
                 }
                 await Task.Delay(1000);
