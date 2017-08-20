a9989fd47fab8fb7c0491a901b8fefada93274e0
John Pomeroy <jpcoded@gmail.com>
2017-08-20 13:44:59 -0400

    Changed how the liquid is moved so that it's done purely with xaml

--- ArcNumber/LiquidTube.xaml.cs
+++ ArcNumber/LiquidTube.xaml.cs
@@ -9,37 +9,14 @@ namespace ArcNumber
     /// </summary>
     public partial class LiquidTube : UserControl
     {
-        private const double Heightinc = 1;
+
         public LiquidTube()
         {
             InitializeComponent();
-            AsyncUpdateLiquid();
-        }
 
-        private async void AsyncUpdateLiquid()
-        {
+        }
 
       
-
-            while (true)
-            {   if(recLiquid.Height >= 212)
-                {
-                    recLiquid.Height = 0;
-                }
-
-
-                if ((recLiquid.Height + Heightinc) > 212)
-                {
-                    recLiquid.Height = 212.58;
-                }
-                else
-                {
-                    recLiquid.Height += Heightinc;
-                }
-
-                await Task.Delay(100);
-            }
-        }
     }
     
 }
